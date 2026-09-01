71080205df480c4d71baad43d44d4d29ea6fe78b
Aishat Babatunde <aishat.babatunde@pau.edu.ng>
2026-09-01 17:30:57 +0100

    my first code wouldnt run before the first deadline

---
+++ week_2/project/project_1.rs
@@ -0,0 +1,13 @@
+fn main () {
+	let p: f64 = 520_000_000.0;
+	let r: f64 = 10.0;
+	let n: f64 = 5.0;
+
+	// compound interest = A = P(1 + (r /100.0))^n
+	let a = p * (1.0 + (r / 100.0)).powf(n);
+	// compound interest = amount - principal
+	let ci = a - p;
+    println!("Amount after 5 years is {}",a);
+    println!("Compound interest is {}", ci);
+
+}
